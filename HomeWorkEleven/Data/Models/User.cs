6ccb809287c86884f35b4b2c67c41fd4e0c072fa
Abdullo <abdullakhzukhriddinov@gmail.com>
2023-09-26 23:41:24 +0500

    add User

---
+++ HomeWorkEleven/Data/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeWorkEleven.Data.Models;
+
+public class User
+{
+    public User(string name, int age, Role role)
+    {
+        Name = name;
+        Age = age;
+        Role = role;
+    }
+
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Please enter your date of birth")]
+    public int Age { get; set; }
+
+    public Role Role { get; set; }
+}
+
+public class Role
+{
+    public Role(string roleName)
+    {
+        RoleName = roleName;
+    }
+
+    public string RoleName { get; set; }
+}
